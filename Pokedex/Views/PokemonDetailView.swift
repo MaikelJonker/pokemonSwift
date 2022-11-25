dd90c92625b807127819953c557149675e074825
MaikelJonker <43063891+MaikelJonker@users.noreply.github.com>
2022-11-25 13:32:26 +0000

    Update PokemonDetailView.swift
    it do be loading now

--- Pokedex/Views/PokemonDetailView.swift
+++ Pokedex/Views/PokemonDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct PokemonDetailView: View {
     
     let simplePokemon: SimplePokemon
+    @State private var isLoading = true
     @EnvironmentObject var pokemonStore: PokemonStore
     @EnvironmentObject var favourites: Favourites
     var pokemon: Pokemon {
@@ -17,53 +18,68 @@ struct PokemonDetailView: View {
     }
     
     var body: some View {
-        ScrollView{
-            Text("# \(pokemon.id)     \(pokemon.name)").font(.title)
-            AsyncImage(
-                url: simplePokemon.imageUrl,
-                content: { image in
-                    image
-                        .resizable()
-                },
-                placeholder: {
-                    ProgressView()
-                }
-            )
-            .animation(.interpolatingSpring(stiffness: 1, damping: 1).speed(3))
-            .frame(width: 300, height: 300)
-            .onAppear{
-                pokemonStore.loadDetails(id: simplePokemon.id)
-            }
-            
-            VStack{
-                ForEach(pokemon.types) {type in
-                    Text(type.type.name)
-                        .padding(10)
-                        .background(Color.blue.cornerRadius(20))
-                        .padding(5)
-                        .foregroundColor(.white)
-                }
-            }
-            .toolbar() {
-                ToolbarItem(placement: ToolbarItemPlacement.navigationBarTrailing) {
-                    HStack {
-                        Button(
-                            action: {
-                                favourites.toggleFavorite(simplePokemon)
-                            },
-                            label: {
-                                let isFavorite = favourites.isFavorite(simplePokemon)
-                                Image(systemName: isFavorite ? "heart.fill" : "heart")
+        VStack{
+            if(isLoading){
+                ProgressView()
+            } else {
+                ScrollView{
+                    Text("# \(pokemon.id)     \(pokemon.name)").font(.title)
+                    AsyncImage(
+                        url: simplePokemon.imageUrl,
+                        content: { image in
+                            image
+                                .resizable()
+                        },
+                        placeholder: {
+                            ProgressView()
+                        }
+                    )
+                    .animation(.interpolatingSpring(stiffness: 1, damping: 1).speed(3))
+                    .frame(width: 300, height: 300)
+                    .onAppear{
+                        pokemonStore.loadDetails(id: simplePokemon.id)
+                    }
+                    
+                    VStack{
+                        ForEach(pokemon.types) {type in
+                            Text(type.type.name)
+                                .padding(10)
+                                .background(Color.blue.cornerRadius(20))
+                                .padding(5)
+                                .foregroundColor(.white)
+                        }
+                    }
+                    .toolbar() {
+                        ToolbarItem(placement: ToolbarItemPlacement.navigationBarTrailing) {
+                            HStack {
+                                Button(
+                                    action: {
+                                        favourites.toggleFavorite(simplePokemon)
+                                    },
+                                    label: {
+                                        let isFavorite = favourites.isFavorite(simplePokemon)
+                                        Image(systemName: isFavorite ? "heart.fill" : "heart")
+                                    }
+                                )
                             }
-                        )
+                        }
                     }
                 }
             }
         }
+            .onAppear(perform: handleOnAppear)
     }
 }
 
-
+private extension PokemonDetailView {
+    
+    func handleOnAppear() {
+        pokemonStore.loadDetails(id: pokemon.id)
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+            self.isLoading = false
+        }
+    }
+}
 struct PokemonDetailView_Previews: PreviewProvider {
     static var previews: some View {
         PokemonDetailView(simplePokemon: SimplePokemon(id: 1, name: "bulbasaur"))
